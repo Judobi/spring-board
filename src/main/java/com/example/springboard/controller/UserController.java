2038b9f8e0e454bd51bae90aab4cbd7c92611e78
Judobi <snowmelodia@gmail.com>
2024-05-01 00:40:12 +0900

    응답 수정

--- src/main/java/com/example/springboard/controller/UserController.java
+++ src/main/java/com/example/springboard/controller/UserController.java
@@ -5,6 +5,8 @@ import com.example.springboard.dto.UserSignUp;
 import com.example.springboard.global.StatusCode;
 import com.example.springboard.service.UserService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -18,19 +20,22 @@ public class UserController {
         this.userService = userService;
     }
 
+    //todo - 로그인 토큰 구현 후, 로그인 성공시 토큰 발급 추가 필요
     @PostMapping("/login")
-    public void login(@RequestParam(value = "id") String id,
-                      @RequestParam(value = "pw") String pw){
-        System.out.println(id + pw);
+    public ResponseEntity<?> login(@RequestParam(value = "id") @NotBlank String id,
+                      @RequestParam(value = "pw") @NotBlank String pw){
 
         // loginService.login 실행 -> 성공 : user 객체 반환, 실패 : null
         User loginUser = userService.login(id,pw);
         if(loginUser == null){
-            System.out.println("로그인 실패");
-            return;
+            return ResponseEntity
+                    .status(StatusCode.LOGIN_FAIL.getStatus())
+                    .body(StatusCode.LOGIN_FAIL.getMessage());
         }
 
-        System.out.println("123");
+        return ResponseEntity
+                .status(StatusCode.LOGIN_SUCCESS.getStatus())
+                .body(StatusCode.LOGIN_SUCCESS.getMessage());
     }
 
     /**
@@ -38,27 +43,31 @@ public class UserController {
      * @param id 회원가입하고 싶은 아이디
      */
     @GetMapping("/login/{id}")
-    public String checkLoginID(@PathVariable("id") String id){
+    public ResponseEntity<?> checkLoginID(@PathVariable("id") @NotBlank String id){
         if(userService.checkLoginID(id)){
-            return StatusCode.Login_ID_FAIL.getMessage();
+            return ResponseEntity
+                    .status(StatusCode.LOGIN_ID_FAIL.getStatus())
+                    .body(StatusCode.LOGIN_ID_FAIL.getMessage());
         }
-        return StatusCode.LOGIN_ID_SUCCESS.getMessage();
+        return ResponseEntity
+                .status(StatusCode.LOGIN_ID_SUCCESS.getStatus())
+                .body(StatusCode.LOGIN_ID_SUCCESS.getMessage());
     }
 
-    //todo - 리턴 response 객체 만들기 -> UserSignup 속 NotBlank message를 상태코드와 함께 사용자에게 리턴해야함
+
     @PostMapping("/signup")
-    public String signUp(@Valid @RequestBody UserSignUp userSignUp, BindingResult bindingResult){
+    public ResponseEntity<?> signUp(@Valid @RequestBody UserSignUp userSignUp, BindingResult bindingResult){
         if(bindingResult.hasErrors()){
             StringBuilder sb = new StringBuilder();
             bindingResult.getAllErrors().forEach(objectError -> {
                 String msg = objectError.getDefaultMessage();
                 sb.append(msg);
             });
-            return sb.toString();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(sb.toString());
         }
 
         userService.signUp(userSignUp);
-        return userSignUp.toString();
+        return ResponseEntity.status(HttpStatus.CREATED).body(StatusCode.SIGNUP_SUCCESS.getMessage());
     }
 
 
--- src/main/java/com/example/springboard/global/StatusCode.java
+++ src/main/java/com/example/springboard/global/StatusCode.java
@@ -7,9 +7,12 @@ public enum StatusCode {
     SUCCESS(HttpStatus.OK, "요청 성공했습니다."),
     LOGIN_ID_SUCCESS(HttpStatus.OK, "사용가능한 아이디입니다."),
     LOGIN_SUCCESS(HttpStatus.OK, "로그인 성공했습니다."),
+    SIGNUP_SUCCESS(HttpStatus.CREATED, "회원가입이 완료되었습니다"),
 
     // 실패
-    Login_ID_FAIL(HttpStatus.CONFLICT, "이미 사용중인 아이디입니다");
+    LOGIN_FAIL(HttpStatus.BAD_REQUEST, "아이디 또는 비밀번호가 틀렸습니다"),
+    LOGIN_ID_FAIL(HttpStatus.CONFLICT, "이미 사용중인 아이디입니다"),
+    SIGNUP_FAIL(HttpStatus.BAD_REQUEST, "");
 
     private final HttpStatus status;
     private final String message;
